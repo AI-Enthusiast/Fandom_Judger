a022d0ed52fd26efa02fe95b30e4b3231b0f1543
cormac <cormacdacker@gmail.com>
2024-09-26 20:32:13 -0700

    Updated scraper to itterate through the all works page

--- scraper.py
+++ scraper.py
@@ -7,11 +7,16 @@ import random
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-url_prefix = 'https://archiveofourown.org/works/'
-url_suffix = '?view_full_work=true'
-adult_suffix = '&view_adult=true'
+def get_wait_time(quick=True):
+    if quick:
+        return random.uniform(19, 31)
+    else:
+        return random.uniform(30, 60)
 
 def get_story_info(work_id):
+    url_prefix = 'https://archiveofourown.org/works/'
+    url_suffix = '?view_full_work=true'
+    # adult_suffix = '&view_adult=true'
     url = url_prefix + str(work_id) + url_suffix
     raw = requests.get(url)
     bad_count = 0
@@ -120,40 +125,96 @@ def process_work_id(work_id):
     else:
         return [work_id, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]
 
-def rand_scrape(n = 5000, max = 160000000, seed = 42):
-    random.seed(seed)
-    work_ids = random.sample(range(1, max), n)
+# def rand_scrape(n = 5000, max = 160000000, seed = 42):
+#     random.seed(seed)
+#     work_ids = random.sample(range(1, max), n)
+#
+#     old_story_db = pd.read_csv('output/story_db.csv')
+#
+#     work_ids = [i for i in work_ids if i not in old_story_db['work_id'].values]
+#
+#     story_db = []
+#
+#     with ThreadPoolExecutor() as executor:
+#         futures = {executor.submit(process_work_id, work_id): work_id for work_id in work_ids}
+#         for future in tqdm(as_completed(futures), total=len(work_ids), desc="Scrapping works"):
+#             story_db.append(future.result())
+#
+#     # print(story_db)
+#     if len(story_db) == 0:
+#         return
+#     try:
+#         story_db = pd.DataFrame(story_db,
+#                                 columns=['work_id', 'title', 'author', 'summary', 'notes', 'rating', 'warnings', 'categories',
+#                                          'fandoms', 'relationships', 'characters', 'additional_tags', 'language', 'published',
+#                                          'word_count', 'chapter_count', 'comment_count', 'kudos_count', 'bookmarks_count',
+#                                          'hits_count'])
+#         story_db = pd.concat([old_story_db, story_db])
+#
+#         story_db.to_csv('output/story_db.csv', index=False)
+#     except ValueError:
+#         print(story_db)
+#         print('err')
 
-    old_story_db = pd.read_csv('output/story_db.csv')
-
-    work_ids = [i for i in work_ids if i not in old_story_db['work_id'].values]
-
+def multi_scrape_ids(work_ids):
     story_db = []
-
     with ThreadPoolExecutor() as executor:
         futures = {executor.submit(process_work_id, work_id): work_id for work_id in work_ids}
         for future in tqdm(as_completed(futures), total=len(work_ids), desc="Scrapping works"):
             story_db.append(future.result())
+    return story_db
 
-    # print(story_db)
-    if len(story_db) == 0:
-        return
-    try:
-        story_db = pd.DataFrame(story_db,
-                                columns=['work_id', 'title', 'author', 'summary', 'notes', 'rating', 'warnings', 'categories',
-                                         'fandoms', 'relationships', 'characters', 'additional_tags', 'language', 'published',
-                                         'word_count', 'chapter_count', 'comment_count', 'kudos_count', 'bookmarks_count',
-                                         'hits_count'])
-        story_db = pd.concat([old_story_db, story_db])
-
-        story_db.to_csv('output/story_db.csv', index=False)
-    except ValueError:
-        print(story_db)
-        print('err')
+def get_work_ids(url, soup = None):
+    if soup is None:
+        raw = requests.get(url)
+        soup = bs(raw.text, 'html.parser')
+    works = soup.find_all('li', class_='work')
+    work_ids = [work.find('a')['href'].split('/')[-1] for work in works]
+    return work_ids
 
 if __name__ == '__main__':
-    for i in range(10):
-        rand_scrape(seed=42+i, n=5000)
-        wait_time = 30*(i+1)
-        print(f'Waiting for {wait_time} seconds')
-        time.sleep(wait_time)
+    # for i in range(10):
+    #     rand_scrape(seed=42+i, n=5000)
+    #     wait_time = 30*(i+1)
+    #     print(f'Waiting for {wait_time} seconds')
+    #     time.sleep(wait_time)
+
+    all_works_url = 'https://archiveofourown.org/works/search?work_search%5Bquery%5D=&work_search%5Btitle%5D=&work_search%5Bcreators%5D=&work_search%5Brevised_at%5D=&work_search%5Bcomplete%5D=&work_search%5Bcrossover%5D=&work_search%5Bsingle_chapter%5D=0&work_search%5Bword_count%5D=&work_search%5Blanguage_id%5D=&work_search%5Bfandom_names%5D=&work_search%5Brating_ids%5D=&work_search%5Bcharacter_names%5D=&work_search%5Brelationship_names%5D=&work_search%5Bfreeform_names%5D=&work_search%5Bhits%5D=&work_search%5Bkudos_count%5D=&work_search%5Bcomments_count%5D=&work_search%5Bbookmarks_count%5D=&work_search%5Bsort_column%5D=_score&work_search%5Bsort_direction%5D=desc&commit=Search'
+    page_prefix = 'https://archiveofourown.org/works/search?commit=Search&page='
+    page_suffix = '&work_search%5Bbookmarks_count%5D=&work_search%5Bcharacter_names%5D=&work_search%5Bcomments_count%5D=&work_search%5Bcomplete%5D=&work_search%5Bcreators%5D=&work_search%5Bcrossover%5D=&work_search%5Bfandom_names%5D=&work_search%5Bfreeform_names%5D=&work_search%5Bhits%5D=&work_search%5Bkudos_count%5D=&work_search%5Blanguage_id%5D=&work_search%5Bquery%5D=&work_search%5Brating_ids%5D=&work_search%5Brelationship_names%5D=&work_search%5Brevised_at%5D=&work_search%5Bsingle_chapter%5D=0&work_search%5Bsort_column%5D=_score&work_search%5Bsort_direction%5D=desc&work_search%5Btitle%5D=&work_search%5Bword_count%5D='
+    page = 2
+    first = True
+    # get the number of works
+    raw = requests.get(all_works_url)
+    soup = bs(raw.text, 'html.parser')
+    num_works = soup.find('h3', class_='heading').text
+    num_works = num_works.split(' ')[0]
+    num_works_int = int(num_works.replace(',', ''))
+    num_of_pages = num_works_int // 20 + 1
+    for i in range(num_of_pages):
+        old_db = pd.read_csv('output/story_db.csv')
+        if first:
+            work_ids = get_work_ids(all_works_url, soup)
+            first = False
+        else:
+            work_ids = get_work_ids(page_prefix + str(page) + page_suffix)
+            page += 1
+        story_db = multi_scrape_ids(work_ids)
+
+        if len(story_db) == 0:
+            continue
+
+        try:
+            story_db = pd.DataFrame(story_db,
+                                    columns=['work_id', 'title', 'author', 'summary', 'notes', 'rating', 'warnings', 'categories',
+                                            'fandoms', 'relationships', 'characters', 'additional_tags', 'language', 'published',
+                                            'word_count', 'chapter_count', 'comment_count', 'kudos_count', 'bookmarks_count',
+                                            'hits_count'])
+            story_db = pd.concat([old_db, story_db])
+            story_db.to_csv('output/story_db.csv', index=False)
+
+        except ValueError:
+            print(story_db)
+            print('err')
+
+        time.sleep(get_wait_time(False))
