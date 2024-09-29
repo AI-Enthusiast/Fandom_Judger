e6c5e758098ea9a349af8563b1a17b174d315602
cormac <cormacdacker@gmail.com>
2024-09-28 22:40:56 -0700

    functional programing

--- output_cleanup.py
+++ output_cleanup.py
@@ -2,24 +2,28 @@ import pandas as pd
 import glob
 import os
 
-# read in story_db.csv
-story_db = pd.read_csv('output/story_db.csv')
+def read_story_db(file_path):
+    return pd.read_csv(file_path)
 
-# get the list of txt files in the output/stories directory
-story_files = glob.glob('output/stories/*.txt')
+def remove_files_not_in_db(story_db, directory):
+    story_files = glob.glob(os.path.join(directory, '*.txt'))
+    for story_file in story_files:
+        work_id = int(os.path.basename(story_file).replace('.txt', ''))
+        if work_id not in story_db['work_id'].values:
+            os.remove(story_file)
+            print(f'Removed {story_file}')
 
-# remove any files that are not in the story_db
-for story_file in story_files:
-    work_id = int(os.path.basename(story_file).replace('.txt', ''))
-    if work_id not in story_db['work_id'].values:
-        os.remove(story_file)
-        print(f'Removed {story_file}')
+def clean_story_db(story_db):
+    story_db = story_db.dropna(subset=['title', 'author', 'summary', 'notes', 'rating', 'warnings', 'categories', 'fandoms', 'relationships', 'characters', 'additional_tags', 'language', 'published', 'word_count', 'chapter_count', 'comment_count', 'kudos_count', 'bookmarks_count', 'hits_count'])
+    story_db = story_db.sort_values('hits_count', ascending=False).drop_duplicates('work_id')
+    return story_db
 
-# in the story_db drop all rows where everything but work_id is None
-story_db = story_db.dropna(subset=['title', 'author', 'summary', 'notes', 'rating', 'warnings', 'categories', 'fandoms', 'relationships', 'characters', 'additional_tags', 'language', 'published', 'word_count', 'chapter_count', 'comment_count', 'kudos_count', 'bookmarks_count', 'hits_count'])
+def save_story_db(story_db, file_path):
+    story_db.to_csv(file_path, index=False)
 
-# order the story_db by hits_count then deduplicate by work_id
-story_db = story_db.sort_values('hits_count', ascending=False).drop_duplicates('work_id')
-
-# write the story_db back to a csv
-story_db.to_csv('output/story_db.csv', index=False)
+if __name__ == '__main__':
+    story_db_path = 'output/story_db.csv'
+    story_db = read_story_db(story_db_path)
+    remove_files_not_in_db(story_db, 'output/stories')
+    story_db = clean_story_db(story_db)
+    save_story_db(story_db, story_db_path)
